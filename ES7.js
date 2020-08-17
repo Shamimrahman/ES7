3dba3f3c679dd2adaf6e43559f01d9aa085b6f37
unknown <shamimrocky801@yahoo.com>
2020-08-17 21:05:04 +0600

    ES7

---
+++ ES7.js
@@ -0,0 +1,43 @@
+// ES7
+
+//includes
+//Exponential
+
+
+//include search er moto kaj kore thake.
+
+let party=["Shamim","Tuli","A"]
+
+party.includes("Shamim") //return True; 
+
+// includes er use : Suppose ami amar basay ekta party arrange korsi and jara jara invited tarai shudhu join korte parbe so ami aita kivabe bujbo ?
+
+//here its the way 
+
+let name=prompt("What is your name")
+
+const check=party.includes(name)
+
+if(check== true)
+{
+	alert("You are invited")
+}
+
+else{
+	alert("YOu are not")
+}
+
+//Exponential math.power er bodole use kora hoy
+
+//previous way
+
+var a=math.pow(2,3) 
+
+a// return 8
+
+//new way
+
+var a=2**3 //return 8..aikhn a star 2 tai hoilo Exponential
+
+
+
